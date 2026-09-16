2408298986bf7a7980083fe43cdc0bb1917a7ecc
beginner-cryptonyx <mayanksethi242008@gmail.com>
2026-09-16 23:59:24 +0530

    Made gitleaksscanner work

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-function App() {
+import ScanInput from "./components/ScanInput";
+import { scanText } from "./scanners";
+import gitleaksScan from "./scanners/gitleaksScanner";
 
+function App() {
+  console.log(gitleaksScan("A3-ABC123-ABCDEFGHIJK-ABCDE-12345-Z9X8W"))
   return (
     <>
+      <div className="App">
+        <h1>AI PreFlight</h1>
+        {/* <ScanInput /> */}
+      </div>
+
     </>
   )
 }
--- src/scanners/gitleaksScanner.ts
+++ src/scanners/gitleaksScanner.ts
@@ -1,12 +1,49 @@
 import { load } from "js-toml";
-import regex from "./regex/gitleaks.toml?raw";
+import regexFile from "./regex/gitleaks.toml?raw";
+import { type Finding } from "./types";
+import { RE2Set, RE2JS } from "re2js";
 
-export default function gitleaksScan() {
+interface rule {
+  description: string;
+  entropy: number;
+  id: string;
+  keywords: string[];
+  regex: string;
+}
+
+const parsedData = load(regexFile);
+const rulesArray = parsedData.rules as rule[];
+
+const regexSet = new RE2Set();
+const compiledSet: { rule: rule; regex: RE2JS }[] = [];
+
+rulesArray.forEach((rule) => {
   try {
-    const parsedData = load(regex);
-    console.log(parsedData);
-    
-  } catch (err) {
-    console.error(err);
+    const regex = RE2JS.compile(rule.regex);
+    regexSet.add(rule.regex);
+    compiledSet.push({ rule: rule, regex: regex });
+  } catch (error) {
+    console.log("could not add", rule.id, error);
   }
+});
+
+export default function gitleaksScan(text: string) {
+  const Violations: Finding[] = [];
+  const matchedIndices = regexSet.match(text);
+
+  for (const i of matchedIndices) {
+    const { rule, regex } = compiledSet[i];
+    const matcher = regex.matcher(text);
+
+    while (matcher.find()) {
+      Violations.push({
+        label: rule.id,
+        severity: "high",
+        match: matcher.group(),
+        start: matcher.start(),
+        end: matcher.end(),
+      } as Finding);
+    }
+  }
+  return Violations
 }
