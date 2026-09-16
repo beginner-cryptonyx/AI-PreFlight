a9d943141b5d6ef2671e89284eaaf461a26a0c7a
beginner-cryptonyx <mayanksethi242008@gmail.com>
2026-09-17 00:34:37 +0530

    Implimented Luhn check and overal credit card check

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import ScanInput from "./components/ScanInput";
 import { scanText } from "./scanners";
+import creditCardScan from "./scanners/creditCardScanner";
 import gitleaksScan from "./scanners/gitleaksScanner";
 
 function App() {
   console.log("TEST-1: API KEY", gitleaksScan("A3-ABC123-ABCDEFGHIJK-ABCDE-12345-Z9X8W"))
-  console.log("TEST-2: CREDIT CARD", gitleaksScan("A3-ABC123-ABCDEFGHIJK-ABCDE-12345-Z9X8W"))
+  console.log("TEST-2: CREDIT CARD", creditCardScan("4111111111111111"))
   return (
     <>
       <div className="App">
--- src/scanners/creditCardScanner.ts
+++ src/scanners/creditCardScanner.ts
@@ -1,16 +1,59 @@
 import { type Finding } from "./types";
 
-const creditCardRegex = /\b(?:\d[ -]*?){13,19}\b/
-const PotantialViolations:{start:number, end:number}[] = []
-const Violations:Finding[] = []
+const creditCardRegex = /\b(?:\d[ -]*?){13,19}\b/g;
 
-function regexCreditCardCheck(text:string){
-    const matches = text.matchAll(creditCardRegex)
+function regexCreditCardCheck(text: string) {
+  const matches = text.matchAll(creditCardRegex);
+const PotantialViolations: { start: number; end: number }[] = [];
 
-    for (const match of matches){
-        const startIndex = match.index;
-        const endIndex = startIndex + match[0].length;
-        PotantialViolations.push({start:startIndex, end:endIndex})
-    }
+
+  for (const match of matches) {
+    const startIndex = match.index;
+    const endIndex = startIndex + match[0].length;
+    PotantialViolations.push({ start: startIndex, end: endIndex });
+  }
+
+  return PotantialViolations
 }
 
+function Luhn(text: string) {
+  const digits = text.replace(/\D/g, "");
+
+  let sum = 0;
+  let doubleDigit = false;
+
+  for (let i = digits.length - 1; i >= 0; i--) {
+    let digit = Number(digits[i]);
+
+    if (doubleDigit) {
+      digit *= 2;
+
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+
+    sum += digit;
+    doubleDigit = !doubleDigit;
+  }
+
+  return sum % 10 === 0;
+}
+
+export default function creditCardScan(text:string):Finding[]{
+const Violations: Finding[] = [];
+const Candidates:{ start: number; end: number }[] = regexCreditCardCheck(text)
+
+for (const candidate of Candidates){
+    const TrialText = text.slice(candidate.start, candidate.end)
+    if (Luhn(TrialText)) {
+        Violations.push({
+            label: "Credit card number",
+            severity: "critical",
+            start: candidate.start,
+            end: candidate.end
+        } as Finding)
+    }
+}
+return Violations
+}
