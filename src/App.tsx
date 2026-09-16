00b970aeb719c9a1799c86d3e032f09cff110187
beginner-cryptonyx <mayanksethi242008@gmail.com>
2026-09-17 00:49:58 +0530

    Implimented a proper way to test functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import ScanInput from "./components/ScanInput";
 import { scanText } from "./scanners";
-import creditCardScan from "./scanners/creditCardScanner";
-import gitleaksScan from "./scanners/gitleaksScanner";
+import Test from "./test/test";
+
 
 function App() {
-  console.log("TEST-1: API KEY", gitleaksScan("A3-ABC123-ABCDEFGHIJK-ABCDE-12345-Z9X8W"))
-  console.log("TEST-2: CREDIT CARD", creditCardScan("4111111111111111"))
+  Test()
   return (
     <>
       <div className="App">
---
+++ src/test/test.ts
@@ -0,0 +1,37 @@
+import creditCardScan from "../scanners/creditCardScanner";
+import gitleaksScan from "../scanners/gitleaksScanner";
+import { type Finding } from "../scanners/types";
+
+const TestString = `
+A3-ABC123-ABCDEFGHIJK-ABCDE-12345-Z9X8W
+
+Luhn Positive:
+4111-1111-1111-1111
+4012888888881881
+5555 5555 5555 4444
+378282246310005
+6011-1111-1111-1117
+
+Luhn Negative:
+4111111111111112
+5555555555554445
+4012-8888-8888-1882
+
+Pure Fail:
+4111-1111-1111
+4111 1111 1111 1111 12345 //not this one tho ;)
+`
+export default function Test(){
+    const gls = gitleaksScan(TestString)
+    const ccs = creditCardScan(TestString)
+    console.log("API KEYS FOUND: ", gls, Visualise(TestString, gls))
+    console.log("CREDIT CARDS FOUND: ", ccs, Visualise(TestString, ccs))
+}
+
+function Visualise(text: string, Violations:Finding[]){
+    const slices = []
+    for (const violation of Violations){
+        slices.push(text.slice(violation.start, violation.end))
+    }
+    return slices
+}
